bc0d050c1b318ac91eed740e7473051b579f93d9
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-05-07 11:35:05 +0300

    added templates

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
-    context = {'posts': posts}
-    return render(request, 'blog_app/post/list.html', context)
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    context = {'page': page, 'posts': posts}
+    return render(request,'blog/post/list.html', context)
 
-def post_detail(request, year, month, day):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day) 
     context = {'post': post}
     return render(request, 'blog_app/post/detail.html', context)   
