bdf429bf9887282a28fd9c03c70174c44ca62c6f
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-05-08 21:03:35 +0300

    added share via mail functionality

--- blog/settings.py
+++ blog/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#E-mail setup
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your_account@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- blog/urls.py
+++ blog/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog_app.urls', namespace='index')),
     path('blog_app/', include('blog_app.urls', namespace='blog_app')),
 ]
